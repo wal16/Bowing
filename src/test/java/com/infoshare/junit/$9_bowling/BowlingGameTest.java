55e43ae3dbc6a221f2ea271475c9050b70274de8
Michal Samujlo <michal.samujlo@gmail.com>
2018-01-25 00:02:00 +0100

    add bowling rules

--- src/test/java/com/infoshare/junit/$9_bowling/BowlingGameTest.java
+++ src/test/java/com/infoshare/junit/$9_bowling/BowlingGameTest.java
@@ -2,6 +2,21 @@ package com.infoshare.junit.$9_bowling;
 
 import org.junit.Test;
 
+/*
+
+Punktacja w kręglach:
+
+1. gra ma dziesięć rund (frame)
+2. w każdej rundzie gracz ma dwie rzuty (roll), żeby strącić dziesięć kręgli
+3. gracz zdobywa tyle punktów ile strącił kręgli, plus bonusy za “strike” i “spare”
+4. “strike” jest wtedy, gdy gracza strąci dziesięć kręgli w jednym rzucie, dostanie wtedy dodatkowo tyle punktów ile strąci kręgli w dwóch następnych rzutach
+5. “spare” to sytuacja, gdy gracz strąci dziesięć kręgli w dwóch rzutach, dodatkowo dostaje wtedy tyle punktów ile strąci kręgli w następnym rzucie
+6. jeżeli gracz zdobędzie “strike” albo “spare” w dziesiątej rundzie to ma dwa (jeden w przypadku “spare”) dodatkowe rzuty na zdobycie dodatkowych punktów
+7. punkty zdobyte po ostatniej rundzie naliczane są tylko raz
+
+@see http://www.bowlinggenius.com
+
+*/
 public class BowlingGameTest {
 
     @Test
